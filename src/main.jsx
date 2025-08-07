0ae4b8ab10d9523188a4e093154a2f7882135d94
guilhermeborim <borimdev@gmail.com>
2025-08-07 09:26:45 -0300

    setup inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router";
+import App from "./App.jsx";
+import "./index.css";
+import routes from "./routes/routes";
+import store from "./slices/store";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={routes}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </RouterProvider>
+  </StrictMode>
+);
---
+++ src/routes/routes.js
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+
+const routes = createBrowserRouter([{ path: "/", Component: App }]);
+
+export default routes;
