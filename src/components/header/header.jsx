c8d6973aced0535c08d1796634082d7df1852d25
guilhermeborim <borimdev@gmail.com>
2025-08-08 19:06:59 -0300

    badge no carrinho para mostrar quantidade de itens

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,28 +1,33 @@
 import { ShoppingCart } from "lucide-react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router";
 import Logo from "../../assets/marvel.png";
 import { toggleCart } from "../../slices/cart/slice";
-import { Section, Title } from "./styles";
+import { Badge, ButtonWrapper, Section, Title } from "./styles";
 
 function Header() {
   const dispatch = useDispatch();
+  const { items } = useSelector((state) => state.cart);
+  const totalItems = items.reduce((acc, item) => acc + item.quantity, 0);
 
   const openCart = () => {
     dispatch(toggleCart());
   };
+
   return (
     <header>
       <Section>
         <Link to={"/"}>
           <img src={Logo} alt="Logo Marvel" width={120} height={100} />
         </Link>
-        <ShoppingCart
-          size={28}
-          style={{ cursor: "pointer" }}
-          onClick={openCart}
-          aria-label="Abrir carrinho"
-        />
+        <ButtonWrapper onClick={() => openCart()}>
+          <ShoppingCart
+            size={28}
+            style={{ cursor: "pointer" }}
+            aria-label="Abrir carrinho"
+          />
+          {totalItems > 0 && <Badge>{totalItems}</Badge>}
+        </ButtonWrapper>
       </Section>
       <Title>
         Venha conhecer e comprar as novas Histórias em Quadrinhos da Marvel
--- src/components/header/styles.jsx
+++ src/components/header/styles.jsx
@@ -14,3 +14,25 @@ export const Title = styled.h1`
   text-align: center;
   padding-top: 16px;
 `;
+
+export const ButtonWrapper = styled.button`
+  position: relative;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 1.8rem;
+`;
+
+export const Badge = styled.span`
+  position: absolute;
+  top: -5px;
+  right: -8px;
+  background: #e62429;
+  color: white;
+  font-size: 0.75rem;
+  font-weight: bold;
+  padding: 2px 6px;
+  border-radius: 50%;
+  min-width: 18px;
+  text-align: center;
+`;
